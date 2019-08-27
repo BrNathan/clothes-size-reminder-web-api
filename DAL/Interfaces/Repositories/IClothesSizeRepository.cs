c700d391dddb11c43dddf329aa4646e89a3e0a72
Nathan Bruet <nathan.bruet@gmail.com>
2019-08-27 08:39:24 +0200

    Add repository interface

---
+++ DAL/Interfaces/Repositories/IClothesSizeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Models
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Interfaces.Repositories
+{
+    public interface IClothesSizeRepository : IRepositoryBase<ClothesSize>
+    {
+    }
+}
